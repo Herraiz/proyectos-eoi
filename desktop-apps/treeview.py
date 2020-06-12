6fc333efb5f99bd96d45118218dacdf7c1d5918f
Rober Herraiz <herraiz92@gmail.com>
2020-06-12 14:55:57 +0100

    working. I have to solve some problems with the upper bar

---
+++ desktop-apps/treeview.py
@@ -0,0 +1,134 @@
+from os.path import expanduser
+from PyQt5.QtGui import QKeySequence, QTextDocument, QFont
+from PyQt5.QtWidgets import *
+
+home = expanduser('..')
+
+app = QApplication([])
+app.setApplicationName('PyFileBrowser')
+
+#### inicio pynotepad
+
+
+editor = QPlainTextEdit()
+editor.document().setDefaultFont(QFont("monospace"))
+
+def ask_for_confirmation():
+    answer = QMessageBox.question(window2, "Confirm closing",
+                "You have unsaved changes. Are you sure you want to exit?", 
+                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+    return answer
+
+class MyMainWindow(QMainWindow):
+    def closeEvent(self, e):
+        if not editor.document().isModified():
+            return
+        answer = ask_for_confirmation()
+        if answer == QMessageBox.Save:
+            if not save():
+                e.ignore()
+        elif answer == QMessageBox.Cancel:
+            e.ignore()
+
+# IMPORTANTE
+
+window2 = MyMainWindow()
+# window.setWindowTitle("PyNotepad")
+window2.setCentralWidget(editor)
+
+file_menu = window2.menuBar().addMenu("&File")
+file_path = None
+
+def new_document():
+    global file_path
+    if editor.document().isModified():
+        answer = ask_for_confirmation()
+        if answer == QMessageBox.Save:
+            if not save():
+                return
+        elif answer == QMessageBox.Cancel:
+            return
+    editor.clear()
+    file_path = None
+
+
+new_action = QAction("&New document")
+new_action.triggered.connect(new_document)
+new_action.setShortcut(QKeySequence.New)
+file_menu.addAction(new_action)
+
+def show_open_dialog():
+    global file_path
+    filename, _ = QFileDialog.getOpenFileName(window2, 'Open...')
+    if filename:
+        file_contents = ""
+        with open(filename, 'r') as f:
+            file_contents = f.read()
+        editor.setPlainText(file_contents)
+        file_path = filename
+
+open_action = QAction("&Open file...")
+open_action.triggered.connect(show_open_dialog)
+open_action.setShortcut(QKeySequence.Open)
+file_menu.addAction(open_action)
+
+def save():
+    if file_path is None:
+        return show_save_dialog()
+    else:
+        with open(file_path, 'w') as f:
+            f.write(editor.toPlainText())
+        editor.document().setModified(False)
+        return True
+
+def show_save_dialog():
+    global file_path
+    filename, _ = QFileDialog.getSaveFileName(window2, 'Save as...')
+    if filename:
+        file_path = filename
+        save()
+        return True
+    return False
+
+
+save_action = QAction("&Save")
+save_action.triggered.connect(save)
+save_action.setShortcut(QKeySequence.Save)
+file_menu.addAction(save_action)
+
+close_action = QAction("&Close")
+close_action.triggered.connect(window2.close)
+close_action.setShortcut(QKeySequence.Quit)
+file_menu.addAction(close_action)
+
+def show_about_dialog():
+    text = """
+        <center>
+            <h1>PyNotepad</h1><br/>
+            <img src=logo.png width=200 height=200>
+        </center>
+        <p>Version 0.0.1</p>
+    """
+    QMessageBox.about(window2, "About PyNotepad", text)
+
+
+help_menu = window2.menuBar().addMenu("&Help")
+about_action = QAction("&About")
+about_action.triggered.connect(show_about_dialog)
+help_menu.addAction(about_action)
+
+#### fin pynotepad
+model = QDirModel()
+window = QWidget()
+view = QTreeView()
+view.setModel(model)
+view.setRootIndex(model.index(home))
+layout = QHBoxLayout()
+layout.addWidget(view)
+layout.addWidget(window2)  # --- añadir editor
+window.setLayout(layout)
+
+
+window.resize(1000, 900)
+window.show()
+app.exec()

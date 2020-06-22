0ef72ccb0c3e30c2d4795597ce209db97bf6b974
Rober Herraiz <herraiz92@gmail.com>
2020-06-22 14:36:15 +0100

    working open and copy path from context menu

---
+++ desktop-apps/edit_item.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import (
+        Qt, QAbstractTableModel, QVariant)
+
+app = QApplication(sys.argv)
+treeView = QTreeView()
+fileSystemModel = QFileSystemModel(treeView)
+fileSystemModel.setReadOnly(False)
+root = fileSystemModel.setRootPath('.')
+treeView.setModel(fileSystemModel)
+treeView.setRootIndex(root)
+treeView.show()
+app.exec_()
--- desktop-apps/main.py
+++ desktop-apps/main.py
@@ -1,12 +1,13 @@
-from os.path import expanduser
+import os
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeySequence, QTextDocument, QFont, QCursor
 from PyQt5.QtWidgets import (QDockWidget, QPlainTextEdit, QDirModel,
 							 QTreeView, QDockWidget, QFileDialog,
 							 QMainWindow, QApplication, QAction,
-							 QMessageBox, QHeaderView, QMenu)
-from PyQt5.QtCore import Qt
+							 QMessageBox, QHeaderView, QMenu, QFileSystemModel)
+
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -28,7 +29,7 @@ class MainWindow(QMainWindow):
 		self.editor.zoomIn(3)
 
 		# Creating Treeview
-		self.home = expanduser('..')
+		self.home = os.path.expanduser('..')
 		self.model = QDirModel()
 		self.explorer = QTreeView()
 		self.explorer.setSortingEnabled(True) # allows to order by clicking on headers
@@ -53,38 +54,38 @@ class MainWindow(QMainWindow):
 		dockWidget.setFloating(False)
 
 
-		### ACTIONS
+		# ACTIONS
 		
 		## EXPLORER
 
-		# Double click
+		### Double click
 		self.explorer.doubleClicked.connect(self.on_double_click)
 
-		# Right click
+		### Right click
 
 		self.explorer.customContextMenuRequested.connect(self.custom_menu)
 
 		## MENU BAR
 
-		# New document
+		### New document
 		self.new_action = QAction("&New document")
 		self.new_action.triggered.connect(self.new_document)
 		self.new_action.setShortcut(QKeySequence.New)
 		self.file_menu.addAction(self.new_action)
 
-		# Open document
+		### Open document
 		self.open_action = QAction("&Open file...")
 		self.open_action.triggered.connect(self.show_open_dialog)
 		self.open_action.setShortcut(QKeySequence.Open)
 		self.file_menu.addAction(self.open_action)
 
-		# Save document
+		### Save document
 		self.save_action = QAction("&Save")
 		self.save_action.triggered.connect(self.save)
 		self.save_action.setShortcut(QKeySequence.Save)
 		self.file_menu.addAction(self.save_action)
 
-		# Close PyFileBrowser
+		### Close PyFileBrowser
 		self.close_action = QAction("&Close")
 		self.close_action.triggered.connect(self.close)
 		self.close_action.setShortcut(QKeySequence.Quit)
@@ -104,17 +105,17 @@ class MainWindow(QMainWindow):
 			e.ignore()
 	
 	def on_double_click(self, index):
-		'''
-		This function get the file path of the double clicked item and if it
+
+		''' This function get the file path of the double clicked item and if it
 		meet the given requirements, open that file on the editor widget.
 
 		Attention! We must use self.explorer_file_path becouse if 
 		self.editor.document().isModified() is True, the self.ask_for_confirmation 
 		will use the self.file_path variable and when you double click, 
-		you set a value for the variable.
-		'''
+		you set a value for the variable. '''
 
 		self.explorer_file_path = self.model.filePath(index)
+
 		# If you double click on a directory or an image, nothing will happend
 		try:
 			self.open_from_explorer()
@@ -124,45 +125,32 @@ class MainWindow(QMainWindow):
 			return
 	
 	def open_from_explorer(self):
-		'''
-		This function open the file on the current self.file_path in the editor
-		'''
-		if self.editor.document().isModified():
-			answer = self.ask_for_confirmation()
-			if answer == QMessageBox.Save:
-				if not self.save():
-					return
-			elif answer == QMessageBox.Cancel:
-				return
+
+		''' This function open the file on the current self.file_path in the editor '''
+		
+		self.safe_close()
 		file_contents = ""
 		# self.explorer_file_path = self.file_path
 		with open(self.explorer_file_path, 'r') as f:
 			file_contents = f.read()
 		self.editor.setPlainText(file_contents)
-		# We save the file_path again so that self.save () works correctly	
+		# We save the file_path again so that self.save() works correctly	
 		self.file_path = self.explorer_file_path 
 
 	def new_document(self):
-		'''
-		This function creates a new document and saves if another one is open
-		 and have changes
-		'''
-		if self.editor.document().isModified():
-			answer = self.ask_for_confirmation()
-			if answer == QMessageBox.Save:
-				if not self.save():
-					return
-			elif answer == QMessageBox.Cancel:
-				return
+
+		''' This function creates a new document and saves if another one is open
+		 and have changes '''
+
+		self.safe_close()
 		self.editor.clear()
 		self.file_path = None
 
 
 	def save(self):
-		'''
-		Standard save function. Will ask the file_path throught a dialog
-		if there is no file_path
-		'''
+
+		''' Standard save function. Will ask the file_path throught a dialog
+		if there is no file_path '''
 
 		if self.file_path is None:
 			return self.show_save_dialog()
@@ -183,6 +171,18 @@ class MainWindow(QMainWindow):
 			self.save()
 			return True
 		return False
+
+	def safe_close(self):
+
+		''' If the document is modified, call ask_for_confirmation '''
+
+		if self.editor.document().isModified():
+			answer = self.ask_for_confirmation()
+			if answer == QMessageBox.Save:
+				if not self.save():
+					return
+			elif answer == QMessageBox.Cancel:
+				return
 
 
 	def ask_for_confirmation(self): 
@@ -212,41 +212,103 @@ class MainWindow(QMainWindow):
 
 		''' Custom Context Menu function with some actions '''
 
-		# Creamos el menú
+		# Creating the context menu
 		menu = QMenu(self)
-		self.event = event
-		print(self.event)
-		# Abrimos el fichero
+
+		self.event = event # TODO: Guardamos el event a ver si hace falta
+
+		##ACTIONS
+
+		## Opening the document
 		open_action = QAction("&Abrir fichero")
 		open_action.triggered.connect(self.menu_open_file)
 		menu.addAction(open_action)
 
-		menu.addAction("Renombrar")
-		menu.addAction("Borrar")
-		menu.addAction("Copiar ruta en el portapapeles")
+		## Rename file
+		rename_action = QAction("&Renombrar")
+		rename_action.triggered.connect(self.menu_rename_file)
+		menu.addAction(rename_action)
 
-		# Abrimos el menu en la posición del cursor	
+		## Copy file path to clipboard
+		copy_action = QAction("&Copiar ruta en el portapapeles")
+		copy_action.triggered.connect(self.menu_copy_file_path)
+		menu.addAction(copy_action)
+
+		## Copy file path to clipboard
+		delete_action = QAction("&Borrar")
+		delete_action.triggered.connect(self.menu_delete_file)
+		menu.addAction(delete_action)
+
+
+		# Opening the context menu at the cursor position
 		menu.exec_(QCursor.pos())
 
-	def menu_open_file(self): # TODO: NO FUNCIONA
 
-		'''
-		With open the selected file on the editor if it's not a 
-		directory or a image file
-		'''
 
-		self.explorer_file_path = self.model.filePath(self.event)
-		if self.editor.document().isModified():
-			answer = self.ask_for_confirmation()
-			if answer == QMessageBox.Save:
-				if not self.save():
-					return
-			elif answer == QMessageBox.Cancel:
-				return
-		file_contents = ""
-		with open(self.explorer_file_path, 'r') as f:
-			file_contents = f.read()
-		self.editor.setPlainText(file_contents)	
+	def menu_open_file(self):
+
+		''' With open the selected file on the editor if it's not a 
+		directory or a image file '''
+
+		# Extracting the file_path (as explorer_file_path) at the selected item
+		self.explorer_file_path = self.model.filePath(self.explorer.selectedIndexes()[0])
+		
+		# If you try to open a directory or an image, nothing will happend
+		try:
+			self.open_from_explorer()
+		except IsADirectoryError:
+			return
+		except UnicodeDecodeError:
+			return
+
+	def menu_rename_file(self): # TODO: NO RULA
+		
+		self.explorer_file_path = self.model.filePath(self.explorer.selectedIndexes()[0])
+		# self.model.setReadOnly(False) # hace que se pueda editar, pero no te lo selecciona
+		self.model.selectedIndexes().setReadOnly(False)
+
+		# self.root = self.fileSystemModel.setRootPath('.')
+		# self.explorer.setModel(fileSystemModel)
+		# self.explorer.setRootIndex(root)
+
+	def menu_delete_file(self): # TODO: no rula
+		
+		''' Delete selected file '''
+
+		self.explorer_file_path = self.model.filePath(self.explorer.selectedIndexes()[0])
+
+
+		selected_items = self.explorer.selection()
+
+		for selected_item in selected_items:
+			for i in range(len(self.tasks)):
+				if self.tasks[i].subject == self.explorer.item(selected_item)['values'][0]:
+					self.tasks.pop(i)
+					break
+			self.explorer.delete(selected_item)
+
+
+
+		# self.model.beginRemoveRows(self.model.index(self.explorer_file_path), 0, 0)
+		# self.model.remove(self.model.index(self.explorer_file_path))
+		# self.model.endRemoveRows()
+
+		# print(self.explorer.rootPath(self.model.index(self.explorer_file_path)))
+		# self.model.remove(self.explorer.selectedIndexes()[0])
+		# print(self.model.index(self.explorer_file_path).row())
+
+		# self.model.remove(self.model.index(self.explorer.selectedIndexes()))
+
+
+	def menu_copy_file_path(self):
+
+		''' Extract the path and paste it on the clipboard '''
+
+		self.explorer_file_path = self.model.filePath(self.explorer.selectedIndexes()[0])
+		app.clipboard().setText(self.explorer_file_path)
+		
+
+		
 
 if __name__ == '__main__':
 

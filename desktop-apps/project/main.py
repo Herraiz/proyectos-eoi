56063c98890344331bd75609187553552bf98d78
Rober Herraiz <herraiz92@gmail.com>
2020-06-26 16:02:48 +0100

    wip refactor - new file menu.py

--- desktop-apps/project/main.py
+++ desktop-apps/project/main.py
@@ -10,6 +10,7 @@ from PyQt5.QtWidgets import (QDockWidget, QPlainTextEdit, QFileSystemModel,
 
 from custom_menu import Custom_menu
 from messages import Message
+from menu import Menu
 
 
 
@@ -24,7 +25,7 @@ class MainWindow(QMainWindow):
 		self.resize(1500, 900)
 
 		# menu bar & file_path
-		self.file_menu = self.menuBar().addMenu("&File")
+		# self.file_menu = self.menuBar().addMenu("&File")
 		self.file_path = None
 
 		# Creating dock widget
@@ -43,7 +44,7 @@ class MainWindow(QMainWindow):
 		self.explorer.setRootIndex(self.root)
 
 		self.custom_menu = Custom_menu(self, self.explorer, self.model, app)
-
+		self.menu = Menu(self, self.explorer, self.editor)
 		# Other custom tweaks
 		self.explorer.setSortingEnabled(True) # allows to order by clicking on headers
 		self.explorer.setMinimumWidth(400) # tweaking the explorer size
@@ -74,7 +75,7 @@ class MainWindow(QMainWindow):
 		## EXPLORER
 
 		### Double click
-		self.explorer.doubleClicked.connect(self.on_double_click)
+		self.explorer.doubleClicked.connect(self.menu.on_double_click)
 
 		### Right click
 
@@ -83,37 +84,37 @@ class MainWindow(QMainWindow):
 		## MENU BAR: FILE MENU
 
 		### New document
-		self.new_action = QAction("&New document")
-		self.new_action.triggered.connect(self.new_document)
-		self.new_action.setShortcut(QKeySequence.New)
-		self.file_menu.addAction(self.new_action)
+		# self.new_action = QAction("&New document")
+		# self.new_action.triggered.connect(self.menu.new_document)
+		# self.new_action.setShortcut(QKeySequence.New)
+		# self.file_menu.addAction(self.new_action)
 
 		### Open document
 		self.open_action = QAction("&Open file...")
-		self.open_action.triggered.connect(self.show_open_dialog)
+		self.open_action.triggered.connect(self.menu.show_open_dialog)
 		self.open_action.setShortcut(QKeySequence.Open)
-		self.file_menu.addAction(self.open_action)
+		# self.file_menu.addAction(self.open_action)
 
 		### Save document
 		self.save_action = QAction("&Save")
-		self.save_action.triggered.connect(self.save)
+		self.save_action.triggered.connect(self.menu.save)
 		self.save_action.setShortcut(QKeySequence.Save)
-		self.file_menu.addAction(self.save_action)
+		# self.file_menu.addAction(self.save_action)
 
 		### Close PyFileBrowser
 		self.close_action = QAction("&Close")
-		self.close_action.triggered.connect(self.close)
+		self.close_action.triggered.connect(self.menu.close)
 		self.close_action.setShortcut(QKeySequence.Quit)
-		self.file_menu.addAction(self.close_action)
+		# self.file_menu.addAction(self.close_action)
 
 		## MENU BAR: HELP MENU
 
 		### About
 
-		self.help_menu = self.menuBar().addMenu("&Help")
+		# self.help_menu = self.menuBar().addMenu("&Help")
 		self.about_action = QAction("&About")
 		self.about_action.triggered.connect(self.message.show_about_dialog)
-		self.help_menu.addAction(self.about_action)
+		# self.help_menu.addAction(self.about_action)
 
 
 	def closeEvent(self, e):
@@ -130,88 +131,88 @@ class MainWindow(QMainWindow):
 		elif answer == QMessageBox.Cancel:
 			e.ignore()
 	
-	def on_double_click(self, index): # TODO! not working
+	# def on_double_click(self, index): # TODO! not working
 
-		''' Get the file path of the double clicked item and if it
-		meet the given requirements, open that file on the editor widget.
+	# 	''' Get the file path of the double clicked item and if it
+	# 	meet the given requirements, open that file on the editor widget.
 
-		We must use self.explorer_file_path because if self.editor.document().isModified() 
-		is True, the self.ask_for_confirmation  will use the self.file_path variable 
-		and when you double click, you set a value for the variable. '''
+	# 	We must use self.explorer_file_path because if self.editor.document().isModified() 
+	# 	is True, the self.ask_for_confirmation  will use the self.file_path variable 
+	# 	and when you double click, you set a value for the variable. '''
 
-		self.explorer_file_path = self.model.filePath(index)
+	# 	self.explorer_file_path = self.model.filePath(index)
 
-		# If you double click on a directory or an image, nothing will happend
-		try:
-			self.open_from_explorer()
+	# 	# If you double click on a directory or an image, nothing will happend
+	# 	try:
+	# 		self.open_from_explorer()
 
-		except IsADirectoryError:
-			return # default behavior
+	# 	except IsADirectoryError:
+	# 		return # default behavior
 			
-		except UnicodeDecodeError:
-			self.message.unicode_decode_error()
+	# 	except UnicodeDecodeError:
+	# 		self.message.unicode_decode_error()
 	
 
-	def new_document(self):
+	# def new_document(self):
 
-		''' This function creates a new document and saves if another one is open
-		 and have changes '''
+	# 	''' This function creates a new document and saves if another one is open
+	# 	 and have changes '''
 
-		self.safe_close()
-		self.editor.clear()
-		self.file_path = None
+	# 	self.safe_close()
+	# 	self.editor.clear()
+	# 	self.file_path = None
 
 
-	def save(self):
+	# def save(self):
 
-		''' Standard save function. Will ask the file_path throught a dialog
-		if there is no file_path '''
+	# 	''' Standard save function. Will ask the file_path throught a dialog
+	# 	if there is no file_path '''
 
-		if self.file_path is None:
-			return self.show_save_dialog()
-		else:
-			with open(self.file_path, 'w') as f:
-				f.write(self.editor.toPlainText())
-			self.editor.document().setModified(False)
-			return True
+	# 	if self.file_path is None:
+	# 		return self.show_save_dialog()
+	# 	else:
+	# 		with open(self.file_path, 'w') as f:
+	# 			f.write(self.editor.toPlainText())
+	# 		self.editor.document().setModified(False)
+	# 		return True
 	
 
-	def show_save_dialog(self):
+	# def show_save_dialog(self):
 
-		''' Show the save dialog is there is no file_path'''
+	# 	''' Show the save dialog is there is no file_path'''
 
-		self.filename, _ = QFileDialog.getSaveFileName(self, 'Save as...')
-		if self.filename:
-			self.file_path = self.filename
-			self.save()
-			return True
-		return False
+	# 	self.filename, _ = QFileDialog.getSaveFileName(self, 'Save as...')
+	# 	if self.filename:
+	# 		self.file_path = self.filename
+	# 		self.save()
+	# 		return True
+	# 	return False
 
-	def safe_close(self):
+	# def safe_close(self):
 
-		''' If the document is modified, call message.ask_for_confirmation '''
+	# 	''' If the document is modified, call message.ask_for_confirmation '''
 
-		if self.editor.document().isModified():
-			answer = self.message.ask_for_confirmation()
-			if answer == QMessageBox.Save:
-				if not self.save():
-					return
-			elif answer == QMessageBox.Cancel:
-				return
+	# 	if self.editor.document().isModified():
+	# 		answer = self.message.ask_for_confirmation()
+	# 		if answer == QMessageBox.Save:
+	# 			if not self.save():
+	# 				return
+	# 		elif answer == QMessageBox.Cancel:
+	# 			return
 
 	
 
-	def show_open_dialog(self):
+	# def show_open_dialog(self):
 
-		''' Function for open files saved on your device '''
+	# 	''' Function for open files saved on your device '''
 
-		self.filename, _ = QFileDialog.getOpenFileName(self, 'Open...')
-		if self.filename:
-			file_contents = ""
-			with open(self.filename, 'r') as f:
-				file_contents = f.read()
-			self.editor.setPlainText(file_contents)
-			self.file_path = self.filename
+	# 	self.filename, _ = QFileDialog.getOpenFileName(self, 'Open...')
+	# 	if self.filename:
+	# 		file_contents = ""
+	# 		with open(self.filename, 'r') as f:
+	# 			file_contents = f.read()
+	# 		self.editor.setPlainText(file_contents)
+	# 		self.file_path = self.filename
 
 
 

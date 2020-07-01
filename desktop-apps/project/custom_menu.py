c33a044d30f733888979b5c8552a484d9d075330
Rober Herraiz <herraiz92@gmail.com>
2020-07-01 15:26:31 +0100

    explorer module added and working fine

---
+++ desktop-apps/project/custom_menu.py
@@ -0,0 +1,136 @@
+import os
+import shutil
+
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QMenu, QAction, QMessageBox
+
+from messages import Message
+from menu import Menu
+from explorer import Explorer
+
+
+
+class Custom_menu(QMenu):
+
+    def __init__(self, main, model, editor, app):
+        super().__init__()
+        self.menu = Menu(main, editor, model)
+        self.explorer = Explorer()
+        self.model = model
+        self.main = main
+        self.editor = editor
+        self.app = app
+        self.message = Message()
+        self.index = None
+        self.index_path = None
+
+
+    def context_menu(self, point):
+
+        ''' Custom Context Menu function with some actions '''
+
+        open_action = QAction("Open")
+        open_action.triggered.connect(self.menu_open)
+        self.addAction(open_action)
+
+        rename_action = QAction("Rename")
+        rename_action.triggered.connect(self.menu_rename)
+        self.addAction(rename_action)
+
+        delete_action = QAction("Delete")
+        delete_action.triggered.connect(self.menu_delete_file)
+        self.addAction(delete_action)
+
+        copy_action = QAction("Copy path to clipboard")
+        copy_action.triggered.connect(self.menu_copy_file_path)
+        self.addAction(copy_action)
+
+        self.exec_(QCursor.pos())
+
+
+    def on_double_click(self, index):
+
+        ''' Get the file path of the double clicked item and if it
+        meet the given requirements, open that file on the editor widget '''
+
+        self.index = index
+        self.index_path = self.model.filePath(index)
+
+        try:
+            self.open_from_explorer(self.index_path)
+
+        except IsADirectoryError:
+            return # default behavior
+            
+        except UnicodeDecodeError:
+            self.message.unicode_decode_error()
+
+    def menu_open(self):
+
+        ''' Open the selected file on the editor '''
+
+        # Extracting the file_path (as explorer_file_path)
+        self.index = self.explorer.currentIndex()
+        self.index_path = self.model.filePath(self.index)
+
+        try:
+            self.open_from_explorer(self.index_path)
+
+        except IsADirectoryError:
+            self.explorer.expand(self.index) 
+
+        except UnicodeDecodeError:
+            self.message.unicode_decode_error()
+
+
+    def menu_rename(self):
+
+        ''' Renaming selected file or directory '''
+
+        self.index = self.explorer.currentIndex()
+        self.explorer.edit(self.index)
+
+
+    def menu_delete_file(self):
+        
+        ''' Delete selected file or directory '''
+
+        self.index = self.explorer.currentIndex()
+        self.index_path = self.model.filePath(self.index)
+        filename = self.model.fileName(self.index)
+
+        try:
+            os.remove(self.index_path)
+
+        except IsADirectoryError: # for empty folders
+            try:
+                os.rmdir(self.index_path)
+
+            except OSError: # for remove recursively a directory
+                    answer = self.message.ask_for_delete_confirmation(filename)
+                    if answer == QMessageBox.Yes:
+                        shutil.rmtree(self.index_path)
+                    elif answer == QMessageBox.Cancel:
+                        return
+
+    def menu_copy_file_path(self):
+
+        ''' Extract the path and paste it on the clipboard '''
+        
+        self.index = self.explorer.currentIndex()
+        self.index_path = self.model.filePath(self.index)
+        self.app.clipboard().setText(self.index_path)
+
+
+    def open_from_explorer(self, path):
+
+        ''' This function open the file at the given path on the editor '''
+        
+        self.menu.safe_close()
+        file_contents = ""
+        with open(path, 'r') as f:
+            file_contents = f.read()
+        self.editor.setPlainText(file_contents)
+
+        # We save the file_path again so that save() works correctly	
+        self.main.file_path = path 
---
+++ desktop-apps/project/menu.py
@@ -0,0 +1,103 @@
+
+from PyQt5.QtGui import QCursor, QKeySequence
+from PyQt5.QtWidgets import QMenu, QAction, QMessageBox, QFileDialog
+
+from messages import Message
+from explorer import Explorer
+
+class Menu(QMenu):
+
+    def __init__(self, main, editor, model):
+        super().__init__()
+        self.explorer = Explorer()
+        self.editor = editor
+        self.model = model
+        self.main = main
+        self.message = Message()
+
+        self.menu_actions()
+
+    def menu_actions(self):
+
+        self.new_action = QAction("&New document")
+        self.new_action.triggered.connect(self.new_document)
+        self.new_action.setShortcut(QKeySequence.New)
+        self.main.file_menu.addAction(self.new_action)
+
+        self.open_action = QAction("&Open file...")
+        self.open_action.triggered.connect(self.show_open_dialog)
+        self.open_action.setShortcut(QKeySequence.Open)
+        self.main.file_menu.addAction(self.open_action)       
+
+        self.save_action = QAction("&Save")
+        self.save_action.triggered.connect(self.save)
+        self.save_action.setShortcut(QKeySequence.Save)
+        self.main.file_menu.addAction(self.save_action)
+
+        self.close_action = QAction("&Close")
+        self.close_action.triggered.connect(self.main.close)
+        self.close_action.setShortcut(QKeySequence.Quit)
+        self.main.file_menu.addAction(self.close_action)
+
+        self.about_action = QAction("&About")
+        self.about_action.triggered.connect(self.message.show_about_dialog)
+        self.main.help_menu.addAction(self.about_action)
+
+
+    def new_document(self):
+
+        ''' This function creates a new document and saves if another one is open
+            and have changes '''
+
+        self.safe_close()
+        self.editor.clear()
+        self.main.file_path = None
+
+    def save(self):
+
+        ''' Standard save function. Will ask the file_path throught a dialog
+        if there is no file_path '''
+
+        if self.main.file_path is None:
+            return self.show_save_dialog()
+        else:
+            with open(self.main.file_path, 'w') as f:
+                f.write(self.editor.toPlainText())
+            self.editor.document().setModified(False)
+            return True
+
+
+    def show_save_dialog(self):
+
+        ''' Show the save dialog is there is no file_path'''
+
+        self.filename, _ = QFileDialog.getSaveFileName(self, 'Save as...')
+        if self.filename:
+            self.main.file_path = self.filename
+            self.save()
+            return True
+        return False
+
+    def safe_close(self):
+
+        ''' If the document is modified, call message.ask_for_confirmation '''
+
+        if self.editor.document().isModified():
+            answer = self.message.ask_for_confirmation()
+            if answer == QMessageBox.Save:
+                if not self.save():
+                    return
+            elif answer == QMessageBox.Cancel:
+                return
+
+    def show_open_dialog(self):
+
+        ''' Function for open files saved on your device '''
+
+        self.filename, _ = QFileDialog.getOpenFileName(self, 'Open...')
+        if self.filename:
+            file_contents = ""
+            with open(self.filename, 'r') as f:
+                file_contents = f.read()
+            self.editor.setPlainText(file_contents)
+            self.main.file_path = self.filename

d048a5d17314994c0970e3a6507e1651d6d7f914
Rober Herraiz <herraiz92@gmail.com>
2020-07-14 19:29:28 +0100

    refactor and traslate done. Need more work on 'check if light is...'

--- internet-of-things/main.py
+++ internet-of-things/main.py
@@ -1,54 +1,90 @@
-import ujson as json
-from machine import Pin
 import utime as time
+from machine import Pin
 import network
+import ujson as json
 
-import urequests as requests
 from umqtt.simple import MQTTClient
 
 from credenciales import ssid, password
 from settings import *
 
-led = Pin(2, Pin.OUT)
+class Controller():
 
-led.value(0)
-print('\nConectandose a la wifi...', end='')
-red = network.WLAN(network.STA_IF)
-red.active(True)
-red.connect(ssid, password)
-while not red.isconnected():
-    time.sleep(0.1)
-print('conectado!')
-print(red.ifconfig())
-led.value(1)
+    def __init__(self):
+        self.led = Pin(2, Pin.OUT)
+        self.connect_wifi()
+        self.mqtt_config()
+        self.run()
 
-def sub_cb(topic, msg):
-    topic = topic.decode()
-    msg = msg.decode()
+    def connect_wifi(self):
+        ''' Using credentials on credenciales.py '''
+        self.led.value(0)
+        print('\nConnecting to wifi...', end='')
+        red = network.WLAN(network.STA_IF)
+        red.active(True)
+        red.connect(ssid, password)
+        while not red.isconnected():
+            time.sleep(0.1)
+        print('Connected!')
+        print(red.ifconfig())
+        self.led.value(1)
 
-    print('')
-    print("Me llego por '{}' esto: '{}' ".format(topic, msg))
-    if topic == 'proyectoEOI':
-        if msg == '{“id”: 38350, “value”: 1}':
-            if led.value() == 0:
-                print('Se ha intentado encender la luz pero ya estaba encendida')
+    def mqtt_config(self):
+        ''' Simple MQTT config '''
+        self.client = MQTTClient(ID, MQTT_SERVER)
+        self.client.connect()
+        self.client.set_callback(self.sub_cb)
+        self.client.subscribe(TOPIC)
+
+    def run(self):
+        print('Power on, setup and connection complete')
+        print('Listening on topic: {}'.format(TOPIC.decode()))
+        while True:
+            self.client.check_msg()
+            time.sleep(5)
+
+    def sub_cb(self, topic, msg):
+        ''' Callback for MQTT suscribed topics '''
+        topic = topic.decode()
+        msg = msg.decode().lower() # we're case insensitive!
+
+        msg_dict = self.load_string_to_json(msg)
+
+        print('')
+        print("I got from '{}' this: '{}' ".format(topic, msg))
+
+        if topic == TOPIC.decode(): # correct topic?
+            if msg_dict['id'] != int(ID): # correct id?
+                print('Incorrect ID')
                 return
-            print('Encendiendo luz')
-            led.value(0)
-        elif msg == '{“id”: 38350, “value”: 0}':
-            if led.value() is 1:
-                print('Se ha intentado apagar la luz pero ya estaba apagada')
-                return
-            print('Apagando luz')
-            led.value(1)
 
+            if msg_dict['value'] == 1: # turn on order
+                # if self.check_if_light_is_already_on():
+                    print('Turning ON the light')
+                    self.led.value(0)
 
-client = MQTTClient(ID, MQTT_SERVER)
-client.connect()
-client.set_callback(sub_cb)
-client.subscribe(TOPIC)
+            elif msg_dict['value'] == 0: # turn off order
+                # if self.check_if_light_is_already_off():
+                    print('Turning OFF the light')
+                    self.led.value(1)
 
-print('Encendido, configuracion y conexion completadas')
-while True:
-    client.check_msg()
-    time.sleep(5)
+    def check_if_light_is_already_on(self):
+        if self.led.value() == 1:
+            print('An attempt was made to turn on the light but it was already on')
+        return False
+
+    def check_if_light_is_already_off(self):
+        if self.led.value() == 0:
+            print('An attempt was made to turn off the light but it was already off')
+        return False
+
+    @staticmethod
+    def load_string_to_json(string):
+        try:
+            loaded_json = json.loads(string)
+        except:
+            print('Error loading message. Is not a json.')
+            return
+        return loaded_json
+
+controller = Controller()
